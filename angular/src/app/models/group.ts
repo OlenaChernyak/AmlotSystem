84c1b6cf6cf5e07727cfa457f6e6edee852fc8cf
Lena <53274322+OlenaChernyak@users.noreply.github.com>
2020-05-25 23:31:11 +0300

    Create group.ts

---
+++ angular/src/app/models/group.ts
@@ -0,0 +1,15 @@
+export class Group {
+
+    public constructor(        
+        public id?: number,
+        public coupon?: number,
+        public companyName?: string,        
+        public country?: string,
+        public amount?: number,
+        public guide?: string,
+        public driver?: string,
+        public date?: Date,
+        public commission?: number,
+
+    ) { }
+}
